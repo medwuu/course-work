37200bdde576c37772527dfef185665ca2ec223e
medwuu <aepegon@gmail.com>
2023-05-25 00:24:54 +0300

    процесс шифрования ещё легче. читайте инструкицю(!)

--- Crypto.cpp
+++ Crypto.cpp
@@ -11,15 +11,14 @@
 для корректной работы именно вашей программы проделайте слудущие шаги:
 1. скачайте "Win64 OpenSSL v1.1.1t Light"(!) (https://slproweb.com/products/Win32OpenSSL.html)
 2. во время установки, в качестве пути установки выбрерите файл с проектом, а также на 3 странице установщика нажмите на
-	"The OpenSSL binaries (/bin) directory". далее просто кликаете "next" и ждёте окончания установки
-3. перейдите по пути "ваш_проект\OpenSSL-Win64\bin", откройте openssl.exe и вставьте туда последовательно(!) команды:
-	"genrsa -out rsa.private 4096",
-	"rsa -in rsa.private -pubout -out rsa.public".
-	после этого, можете закрыть консоль и никгода к ней не возвращаться
-4. замените "StudentsData.txt" (ниже) на название своей БД
+	"The OpenSSL binaries (/bin) directory".
+	далее просто кликаете "next" и ждёте окончания установки
+3. замените "StudentsData.txt" (ниже) на название своей БД
 
- после проделанных действий программа при первом запуске выведет пару ошибок в консоль, а при дальнейших запусках не будет
+ после проделанных действий программа при первом запуске может вывести немного мусора в консоль, а при дальнейших запусках не будет
 
+ ! Перед первым запуском:
+* убедитесь, что ваш файл с базой данных сохранён с кодировкой ANSII(!)
 * не надо ничего переносить, переименовывать и т. д.    делайте это лишь в тех случаях, когда программа не работает корректно со 2 раза
  */
 #define FILENAME "StudentsData.txt"
@@ -60,36 +59,43 @@ void Crypt() {
 	}
 	// открываем и записываем в key.txt сгенерированный ключ, которым шифровали базу данных
 	ofstream file;
-	file.open("key.txt", ios::binary);
+	file.open("OpenSSL-Win64\\bin\\key.txt", ios::binary);
 	file.write(pass, 65);
 	file.close();
 	// шифруем key.txt с помощью ключа "rsa.public"
-	command = "OpenSSL-Win64\\bin\\openssl.exe rsautl -encrypt -inkey OpenSSL-Win64\\bin\\rsa.public -pubin -in key.txt -out key.txt.enc";
+	command = "OpenSSL-Win64\\bin\\openssl.exe rsautl -encrypt -inkey OpenSSL-Win64\\bin\\rsa.public -pubin -in OpenSSL-Win64\\bin\\key.txt -out OpenSSL-Win64\\bin\\key.txt.enc";
 	system(command.c_str());
 	// удаляем незашифрованный файл
-	if (remove("key.txt") != 0) {
+	if (remove("OpenSSL-Win64\\bin\\key.txt") != 0) {
 		cout << "[ERROR] - deleting key.txt" << endl;
 	}
 }
 
 // расшифровать файл
 void Decrypt() {
+	// создаём rsa ключи при их отсутствии (первом запуске программы)
+	ifstream rsa_private("OpenSSL-Win64\\bin\\rsa.private");
+	if (!rsa_private) { system("OpenSSL-Win64\\bin\\openssl.exe genrsa -out OpenSSL-Win64\\bin\\rsa.private 4096"); }
+	rsa_private.close();
+	ifstream rsa_public("OpenSSL-Win64\\bin\\rsa.public");
+	if (!rsa_public) { system("OpenSSL-Win64\\bin\\openssl.exe rsa -in OpenSSL-Win64\\bin\\rsa.private -pubout -out OpenSSL-Win64\\bin\\rsa.public"); }
+	rsa_public.close();
 	// расшифровываем файл key.txt с помощью "rsa.private"
-	string command = "OpenSSL-Win64\\bin\\openssl.exe rsautl -decrypt -inkey OpenSSL-Win64\\bin\\rsa.private -in key.txt.enc -out key.txt";
+	string command = "OpenSSL-Win64\\bin\\openssl.exe rsautl -decrypt -inkey OpenSSL-Win64\\bin\\rsa.private -in OpenSSL-Win64\\bin\\key.txt.enc -out OpenSSL-Win64\\bin\\key.txt";
 	system(command.c_str());
 	// удаляем зашифрованный файл
-	if (remove("key.txt.enc") != 0) {
+	if (remove("OpenSSL-Win64\\bin\\key.txt.enc") != 0) {
 		cout << "[ERROR] - deleting key.txt.enc" << endl;
 	}
 	// открываем key.txt и считываем ключ для расшифровки базы данных
 	char pass[65];
 	ifstream file;
-	file.open("key.txt", ios::binary);
+	file.open("OpenSSL-Win64\\bin\\key.txt", ios::binary);
 	file.read(pass, 65);
 	file.close();
 	pass[64] = '\0';
 	// удаляем файл с ключом
-	if (remove("key.txt") != 0) {
+	if (remove("OpenSSL-Win64\\bin\\key.txt") != 0) {
 		cout << "[ERROR] - deleting key.txt" << endl;
 	}
 	// расшифровываем базу данных
